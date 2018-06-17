dbde2e17ccd50d5de031461ab236aac6d7527df7
fmachado091 <fmachado091@gmail.com>
2018-06-16 23:48:31 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from users import USERS
+import os
+import logging
+import requests
+import json
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+ids = [ str(key) for key, value in USERS.items() ]
+
+INVALID_INPUT = 'Invalid input'
+
+BOT_TOKEN = os.environ['HPBR_BOT_TOKEN']
+
+
+# answer helper
+def answer(update, response):
+    update.message.reply_text(response)
+
+
+# command handlers
+def start(bot, update):
+    """handles /start command"""
+    answer(update, 'Hi!')
+
+
+def help(bot, update):
+    """handles /help command"""
+    answer(update, 'Help me if you can, I\'m feeling down')
+
+
+def ranking(bot, update):
+    """handles /ranking command"""
+    answer(update, 'Ranking!')
+
+
+def bets(bot, update):
+    """handles /bets {bet} command"""
+    text = update.message.text
+    textSplit = text.split()
+
+    if(len(textSplit) != 2):
+        answer(update, INVALID_INPUT)
+        return
+
+    try:
+        match = int(textSplit[1])
+    except ValueError:
+        answer(update, INVALID_INPUT)
+        return
+
+    if match <= 0:
+        answer(update, INVALID_INPUT)
+        return
+
+    r = requests.get(url='https://www.motta.ml/bolao2018/sql/getInfo.php?table=bets')
+
+    bets_list = json.loads(r.text)
+    filtered_bets = [ b for b in bets_list if b['id_user'] in ids and b['id_match'] == str(match) ]
+
+    response = ''.join(b['name'] + ': ' + b['goals_home'] + ' x ' + b['goals_visitor'] + '\n' for b in filtered_bets)
+
+    answer(update, response)
+
+
+def error(bot, update, error):
+    """log errors"""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    """starts bot"""
+    # create the EventHandler
+    updater = Updater(BOT_TOKEN)
+
+    # get dispatcher
+    dispatcher = updater.dispatcher
+
+    # register handlers
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("ranking", ranking))
+    dispatcher.add_handler(CommandHandler("bets", bets))
+    dispatcher.add_handler(CommandHandler("help", help))
+
+    dispatcher.add_error_handler(error)
+
+    # starts bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ users.py
@@ -0,0 +1,11 @@
+USERS = {
+    1: "Felipe Zanon",
+    2: "Nelson Motta",
+    44: "Phetor",
+    47: "Vinebas",
+    48: "Thigo",
+    49: "FM",
+    57: "Lucas Couto",
+    72: "Segundo",
+    87: "Gika"
+}
