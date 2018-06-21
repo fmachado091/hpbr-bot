70077f3d116ff35bc2aad744948874929696ef62
fmachado091 <fmachado091@gmail.com>
2018-06-20 23:47:06 -0300

    add empty /bets handler

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from functools import reduce
 from users import USERS
 from matches import MATCHES
 import os
@@ -21,9 +22,19 @@ INVALID_INPUT = 'Invalid input'
 BOT_TOKEN = os.environ['HPBR_BOT_TOKEN']
 
 
-# answer helper
+# utility functions
 def answer(update, response):
     update.message.reply_text(response)
+
+
+def get_bets():
+    r = requests.get(url='https://www.motta.ml/bolao2018/sql/getInfo.php?table=bets')
+    return json.loads(r.text)
+
+
+def get_last_match(bets):
+    matches = list(map(lambda b: b['id_match'], bets))
+    return reduce((lambda x, y: max(x, y)), matches)
 
 
 # command handlers
@@ -42,36 +53,45 @@ def ranking(bot, update):
     answer(update, 'Ranking!')
 
 
-def bets(bot, update):
-    """handles /bets {bet} command"""
-    text = update.message.text
-    textSplit = text.split()
+def bets_with_no_param(bot, update):
+    """handles /bets command"""
+    bets = get_bets()
+    last_match = get_last_match(bets)
+    bets_with_param(bot, update, last_match, bets)
 
-    if(len(textSplit) != 2):
-        answer(update, INVALID_INPUT)
-        return
 
-    try:
-        match = int(textSplit[1])
-    except ValueError:
-        answer(update, INVALID_INPUT)
-        return
-
-    if match <= 0:
-        answer(update, INVALID_INPUT)
-        return
-
-    r = requests.get(url='https://www.motta.ml/bolao2018/sql/getInfo.php?table=bets')
-
-    bets_list = json.loads(r.text)
-    filtered_bets = [ b for b in bets_list if b['id_user'] in ids and b['id_match'] == match ]
+def bets_with_param(bot, update, match, bets=get_bets()):
+    """handles /bets {match} command"""
+    filtered_bets = [ b for b in bets if b['id_user'] in ids and b['id_match'] == match ]
 
     home_team = MATCHES[match]['team_home']
     visitor_team = MATCHES[match]['team_visitor']
+
     response = '** ' + home_team + ' x ' + visitor_team + ' **\n'
     response += ''.join(b['name'] + ': ' + str(b['goals_home']) + ' x ' + str(b['goals_visitor']) + '\n' for b in filtered_bets)
 
     answer(update, response)
+
+
+def bets(bot, update):
+    """handles /bets [{bets}] command"""
+    text = update.message.text
+    split_text = text.split()
+
+    if (len(split_text) == 1):
+        bets_with_no_param(bot, update)
+    elif (len(split_text) == 2):
+        try:
+            match = int(split_text[1])
+        except ValueError:
+            answer(update, INVALID_INPUT)
+            return
+        if match <= 0:
+            answer(update, INVALID_INPUT)
+            return
+        bets_with_param(bot, update, match)
+    else:
+        answer(update, INVALID_INPUT)
 
 
 def error(bot, update, error):
