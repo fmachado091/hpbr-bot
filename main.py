af7ad86ba25a1a0f59a1f19b64b87e3b1d5e7976
fmachado091 <fmachado091@gmail.com>
2018-06-27 00:45:40 -0300

    refactor and add top handler

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ logger = logging.getLogger(__name__)
 ids = [ key for key, value in USERS.items() ]
 
 INVALID_INPUT = 'Invalid input'
+API_ERROR = 'API error'
 
 BOT_TOKEN = os.environ['HPBR_BOT_TOKEN']
 
@@ -36,6 +37,11 @@ def get_last_match(bets):
     return reduce((lambda x, y: max(x, y)), matches)
 
 
+def format_top_row(r):
+    args = (r['position'], r['name'], r['points'], r['fives'], r['threes'], r['twos'])
+    return '{:3d}. {:16s} - {:3d} pts - {:2d} / {:2d} / {:2d}\n'.format(*args)
+
+
 def format_ranking_row_short(r):
     args = (r['position'], r['name'], r['points'])
     return '{:3d}. {:s} ({:d})\n'.format(*args)
@@ -44,6 +50,10 @@ def format_ranking_row_short(r):
 def format_ranking_row(r):
     args = (r['position'], r['name'], r['points'], r['fives'], r['threes'], r['twos'])
     return '{:3d}. {:14s} - {:3d} pts - {:2d} / {:2d} / {:2d}\n'.format(*args)
+
+
+def format_bets_row(name, goals_home, goals_visitor):
+    return '{:14s}: {:d} x {:d}\n'.format(name, goals_home, goals_visitor)
 
 
 # api request functions
@@ -68,50 +78,77 @@ def help(bot, update):
     answer(update, 'Help me if you can, I\'m feeling down')
 
 
-def ranking(bot, update):
-    """handles /ranking command"""
+def ranking_response(update, is_top):
     text = update.message.text
     split_text = text.split()
 
-    formatter = format_ranking_row
-    if len(split_text) == 2 and split_text[1] == 'short':
-        formatter = format_ranking_row_short
+    if is_top:
+        formatter = format_top_row
+    else:
+        formatter = format_ranking_row
+        if len(split_text) == 2 and split_text[1] == 'short':
+            formatter = format_ranking_row_short
 
     ranking = get_ranking()
+
     response = '```\n'
-    response += '** HPBr no Bolão **\n\n'
-    response += ''.join([ formatter(r) for r in ranking if r['id'] in ids ])
+    if is_top:
+        response += ''.join([ formatter(r) for r in ranking if r['position'] <= 10 ])
+    else:
+        response += ''.join([ formatter(r) for r in ranking if r['id'] in ids ])
     response += '```'
-    answer_markdown(bot, update.message.chat.id, response)
+
+    return response
 
 
-def bets_with_no_param(bot, update):
+def ranking(bot, update):
+    """handles /ranking command"""
+    chat_id = update.message.chat.id
+    response = ranking_response(update, False)
+    answer_markdown(bot, chat_id, response)
+
+
+def top(bot, update):
+    """handles /top command"""
+    chat_id = update.message.chat.id
+    response = ranking_response(update, True)
+    answer_markdown(bot, chat_id, response)
+
+
+def bets_with_no_param():
     """handles /bets command"""
     bets = get_bets()
     last_match = get_last_match(bets)
-    bets_with_param(bot, update, last_match, bets)
+
+    return bets_with_param(last_match, bets)
 
 
-def bets_with_param(bot, update, match, bets=get_bets()):
+def bets_with_param(match, bets=get_bets()):
     """handles /bets {match} command"""
     filtered_bets = [ b for b in bets if b['id_user'] in ids and b['id_match'] == match ]
 
     home_team = MATCHES[match]['team_home']
     visitor_team = MATCHES[match]['team_visitor']
 
-    response = '** ' + home_team + ' x ' + visitor_team + ' **\n'
-    response += ''.join(b['name'] + ': ' + str(b['goals_home']) + ' x ' + str(b['goals_visitor']) + '\n' for b in filtered_bets)
+    response = '```\n'
+    response += 'Match #{:d}'.format(match).center(21, ' ')
+    response += '\n'
+    response += '{:s} x {:s}'.format(home_team, visitor_team).center(21, ' ')
+    response += '\n\n'
+    response += ''.join(format_bets_row(b['name'], b['goals_home'], b['goals_visitor']) for b in filtered_bets)
+    response += '```'
 
-    answer(update, response)
+    return response
 
 
 def bets(bot, update):
-    """handles /bets [{bets}] command"""
+    """handles /bets [{match}] command"""
     text = update.message.text
     split_text = text.split()
+    chat_id = update.message.chat.id
 
     if (len(split_text) == 1):
-        bets_with_no_param(bot, update)
+        answer_markdown(bot, chat_id, bets_with_no_param())
     elif (len(split_text) == 2):
         try:
             match = int(split_text[1])
@@ -121,7 +158,7 @@ def bets(bot, update):
         if match <= 0:
             answer(update, INVALID_INPUT)
             return
-        bets_with_param(bot, update, match)
+        answer_markdown(bot, chat_id, bets_with_param(match))
     else:
         answer(update, INVALID_INPUT)
 
@@ -142,6 +179,7 @@ def main():
     # register handlers
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("ranking", ranking))
+    dispatcher.add_handler(CommandHandler("top", top))
     dispatcher.add_handler(CommandHandler("bets", bets))
     dispatcher.add_handler(CommandHandler("help", help))
 
